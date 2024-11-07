932978d32ff9ccdae8a63c3943ec957587216e4a
0242206 <0242206@hc.ac.je>
2024-11-07 11:27:47 +0000

    comment planning updated

--- index.js
+++ index.js
@@ -5,10 +5,10 @@
 // Get the user's level - UserLevel (string)
 // Get the user's hours of study UserHours (number)
 // Validate the user's input
-// Check if the user has selected a level - check a level has been entered, check aginst
-// Check if the user has provided an email address
-// Check if the user has specified at least one hour of study - cannot be negative
-// Check if the number of hours requested is within the allowed range
+// Check if the user has selected a level - check a level has been entered, check against the allowed levels
+// Check if the user has provided an email address - is this valid?, does it have an @, have they entered and email
+// Check if the user has specified at least one hour of study - must be equal to or greater then 1,
+// Check if the number of hours requested is within the allowed range -less then or equal to max hours for level
 // Calculate the total cost
 // Calculate the total cost
 // Display the total cost to the user
