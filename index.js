bbfdb340c679ddedbc5f536f09cb8c6ec7487358
0242206 <0242206@hc.ac.je>
2024-11-07 11:09:44 +0000

    added a commit plain

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-// javascript
-console.log("hello world");
+// Display a form to the user
+// Include input fields for the user's email address, level, and hours of study
+// Capture user's input on form submission
+// Get the user's email address- UserEmail (string)
+// Get the user's level - UserLevel (string)
+// Get the user's hours of study UserHours (number)
+// Validate the user's input
+// Check if the user has selected a level - check a level has been entered, check aginst
+// Check if the user has provided an email address
+// Check if the user has specified at least one hour of study - cannot be negative
+// Check if the number of hours requested is within the allowed range
+// Calculate the total cost
+// Calculate the total cost
+// Display the total cost to the user
